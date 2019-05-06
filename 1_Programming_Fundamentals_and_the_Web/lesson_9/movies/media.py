776aca5376b1cfe0a90d271a2768ead2d8a97c00
Souvik Banerjee <souvikbanerjee@UCLP1137.local>
2019-05-07 01:12:52 +0530

    Movie website mini project

---
+++ 1_Programming_Fundamentals_and_the_Web/lesson_9/movies/media.py
@@ -0,0 +1,11 @@
+import webbrowser
+
+class Movie():
+    def __init__(self, movie_title, movie_story_line, poster_image, trailer_youtube_link):
+        self.title = movie_title
+        self.storyline = movie_story_line
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube_link
+
+    def show_trailer(self):
+        webbrowser.open(self.trailer_youtube_url)
